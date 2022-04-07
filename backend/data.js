edbee25b161c0c6fdd53551d1311b96f602bc196
Eugene Chua <chuaeugene@hotmail.com>
2022-04-07 20:50:03 +0800

    Add backend

---
+++ backend/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'TokinoSora',
+            category:'Idols',
+            image:'/images/TokinoSora.png',
+            price:190,
+            countInStock: 20,
+            brand:"Gen 0",
+            rating:5,
+            numReviews:60,
+            description:'Goddess'
+        },
+        {
+            _id:'2',
+            name:'Sakura Miko',
+            category:'Idols',
+            image:'/images/SakuraMiko.png',
+            price:160,
+            countInStock: 10,
+            brand:"Gen 0",
+            rating:4.5,
+            numReviews:19,
+            description:'Elite Nyee'
+        },
+        {
+            _id:'3',
+            name:'Roboco-san',
+            category:'Idols',
+            image:'/images/RobocoSan.png',
+            price:170,
+            countInStock: 30,
+            brand:"Gen 0",
+            rating:4,
+            numReviews:14,
+            description:'Robotics;Notes'
+        },
+        {
+            _id:'4',
+            name:'Hoshimachi Suisei',
+            category:'Idols',
+            image:'/images/HoshimachiSuisei.png',
+            price:200,
+            countInStock: 30,
+            brand:"Gen 0",
+            rating:5,
+            numReviews:20,
+            description:'Suisei is cute today'
+        },
+        {
+            _id:'5',
+            name:'Shirakami Fubuki',
+            category:'Idols',
+            image:'/images/ShirakamiFubuki.png',
+            price:180,
+            brand:"Gen 1",
+            countInStock: 0,
+            rating:4.5,
+            numReviews:4,
+            description:'CatFox'
+        },
+        {
+            _id:'6',
+            name:'Natsuiro Matsuri',
+            category:'Idols',
+            image:'/images/NatsuiroMatsuri.png',
+            price:150,
+            countInStock: 89,
+            brand:"Gen 1",
+            rating:4,
+            numReviews:5,
+            description:'God'
+        },
+    ],
+};
+export default data;
